667d4c3147dbc336380dc9dec1914488d58517f4
MIGUEL_ANGEL_MORENO_SOTO <a.20308051280522@cbtis128.edu.mx>
2023-03-27 14:58:46 +0000

    tabBar v2

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,62 @@
 import 'package:flutter/material.dart';
+//import 'item.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        primarySwatch: Colors.deepPurple,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
+  _MyHomePageState createState() => _MyHomePageState();
+} //_MyHomePageStatefull
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), // Creates border
+                color: Colors.amberAccent),
+            indicatorColor: Colors.amberAccent,
+            tabs: [
+              Tab(icon: Icon(Icons.flight)),
+              Tab(icon: Icon(Icons.smoking_rooms)),
+              Tab(icon: Icon(Icons.directions_transit)),
+              Tab(icon: Icon(Icons.directions_car)),
+            ],
+          ),
+          centerTitle: true,
+          title: Text('Cklass'),
+        ),
+        body: TabBarView(
+          children: [
+            Icon(Icons.flight, size: 350),
+            Icon(Icons.smoking_rooms, size: 350),
+            Icon(Icons.directions_transit, size: 350),
+            Icon(Icons.directions_car, size: 350),
+          ],
         ),
       ),
     );
-  }
-}
+  } //Widget
+} //
